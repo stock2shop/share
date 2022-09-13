d5819fdfc6a29742bbd982d31f1f83261e3a9bf7
Arata Yano <aratayano00@gmail.com>
2022-09-13 15:54:14 +0200

    System Image test

---
+++ tests/DTO/SystemImageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Stock2Shop\Tests\Share\DTO;
+
+use PHPUnit\Framework\TestCase;
+use Stock2Shop\Share\DTO;
+
+class SystemImageTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $mockData = [
+            'id' => '1',
+            'active' => true,
+            'src' => 'source1'
+        ];
+        $c = new DTO\SystemImage($mockData);
+        $this->assertSystemImage($c);
+        $c->setSrc('any src will do...');
+        $this->assertSystemImage($c);
+        $c = new DTO\SystemImage([]);
+        $this->assertChannelNull($c);
+    }
+
+    private function assertSystemImage(DTO\SystemImage $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\SystemImage', $c);
+        $this->assertIsString($c->getSrc());
+    }
+
+    private function assertChannelNull(DTO\SystemImage $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\SystemImage', $c);
+    }
+
+}
