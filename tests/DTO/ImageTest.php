a48928fb498ed23c42ebf60ee693ae5a7282d614
Arata Yano <aratayano00@gmail.com>
2022-09-13 15:37:56 +0200

    image test

---
+++ tests/DTO/ImageTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Stock2Shop\Tests\Share\DTO;
+
+use PHPUnit\Framework\TestCase;
+use Stock2Shop\Share\DTO;
+
+class ImageTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $mockData = [
+            'src' => 'source1'
+        ];
+        $c = new DTO\Image($mockData);
+        $this->assertImage($c);
+        $c->setSrc('any src will do...');
+        $this->assertImage($c);
+        $c = new DTO\Image([]);
+        $this->assertChannelNull($c);
+    }
+
+    private function assertImage(DTO\Image $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Image', $c);
+        $this->assertIsString($c->getSrc());
+    }
+
+    private function assertChannelNull(DTO\Image $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Image', $c);
+    }
+
+}
