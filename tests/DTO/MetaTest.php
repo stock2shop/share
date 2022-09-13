83e1506f666e6e4d9421182c20cb8e7071516422
Arata Yano <aratayano00@gmail.com>
2022-09-13 15:39:25 +0200

    Meta test

---
+++ tests/DTO/MetaTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Stock2Shop\Tests\Share\DTO;
+
+use PHPUnit\Framework\TestCase;
+use Stock2Shop\Share\DTO;
+
+class metaTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $mockData = [
+            'key' => 'key',
+            'value' => 'value',
+            'template_name' => 'template_name'
+        ];
+        $c = new DTO\Meta($mockData);
+        $this->assertMeta($c);
+        $c->setKey('new key');
+        $this->assertMeta($c);
+        $c = new DTO\Meta([]);
+        $this->assertChannelNull($c);
+    }
+
+    private function assertMeta(DTO\Meta $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Meta', $c);
+        $this->assertIsString($c->getKey());
+        $this->assertIsString($c->getValue());
+        $this->assertIsString($c->getTemplateName());
+    }
+
+    private function assertChannelNull(DTO\Meta $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Meta', $c);
+    }
+
+}
