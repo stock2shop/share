6f99d689cd07e3b9b591bd35d1b41b164273b066
flumonion <chris@gomedia.co.za>
2022-10-18 11:49:14 +0200

    log dto

---
+++ src/DTO/Log.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stock2Shop\Share\DTO;
+
+use JsonSerializable;
+
+class Log extends DTO implements JsonSerializable, DTOInterface
+{
+    public const LOG_LEVEL_ERROR = 'error';
+    public const LOG_LEVEL_DEBUG = 'debug';
+    public const LOG_LEVEL_INFO = 'info';
+    public const LOG_LEVEL_CRITICAL = 'critical';
+    public const LOG_LEVEL_WARNING = 'warning';
+    private const ALLOWED_LOG_LEVEL = [
+        self::LOG_LEVEL_ERROR,
+        self::LOG_LEVEL_DEBUG,
+        self::LOG_LEVEL_INFO,
+        self::LOG_LEVEL_CRITICAL,
+        self::LOG_LEVEL_WARNING
+    ];
+
+    public ?int $channel_id;
+    public int $client_id;
+    public ?array $context;
+    public ?string $ip;
+    public bool $log_to_es;
+    public string $level;
+    public string $message;
+    public ?string $method;
+    public ?float $metric;
+    public string $origin;
+    public ?string $remote_addr;
+    public ?string $request_path;
+    public ?int $source_id;
+    /** @var string[]|null */
+    public ?array $tags;
+    /** @var string[]|null */
+    public ?array $trace;
+    public ?int $user_id;
+
+    public function __construct(array $data)
+    {
+        $context = LogContext::createArray(self::arrayFrom($data, "context"));
+
+        $this->channel_id   = self::intFrom($data, 'channel_id');
+        $this->client_id    = self::intFrom($data, 'client_id');
+        $this->context      = $this->sortArray($context, "key");
+        $this->ip           = self::stringFrom($data, 'ip');
+        $this->log_to_es    = self::boolFrom($data, 'log_to_es');
+        $this->level        = self::stringFrom($data, 'level');
+        $this->message      = self::stringFrom($data, 'message');
+        $this->method       = self::stringFrom($data, 'method');
+        $this->metric       = self::floatFrom($data, "metric");
+        $this->origin       = self::stringFrom($data, 'origin');
+        $this->remote_addr  = self::stringFrom($data, 'remote_addr');
+        $this->request_path = self::stringFrom($data, 'request_path');
+        $this->source_id    = self::intFrom($data, 'source_id');
+        $this->tags         = self::arrayFrom($data, 'tags');
+        $this->trace        = self::arrayFrom($data, 'trace');
+        $this->user_id      = self::intFrom($data, 'user_id');
+        if (!in_array($this->level, self::ALLOWED_LOG_LEVEL)) {
+            throw new \InvalidArgumentException(sprintf('Invalid log level %s', $this->level));
+        }
+    }
+
+    public function jsonSerialize(): array
+    {
+        return (array)$this;
+    }
+
+    public static function createFromJSON(string $json): Log
+    {
+        $data = json_decode($json, true);
+        return new Log($data);
+    }
+
+    /**
+     * @return Log[]
+     */
+    public static function createArray(array $data): array
+    {
+        $a = [];
+        foreach ($data as $item) {
+            $a[] = new Log((array)$item);
+        }
+        return $a;
+    }
+}
---
+++ tests/DTO/LogTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stock2Shop\Tests\Share\DTO;
+
+use PHPUnit\Framework\TestCase;
+use Stock2Shop\Share\DTO;
+
+class LogTest extends TestCase
+{
+    private string $json;
+
+    protected function setUp(): void
+    {
+        $this->json = '{
+            "channel_id": 1,
+            "client_id": 21,
+            "ip": "21.21.21.21",
+            "log_to_es": false,
+            "level": "error",
+            "message": "A message",
+            "method": null,
+            "metric": 1.23,
+            "origin": "Connector",
+            "remote_addr": "129.168.0.1",
+            "request_path": "/some/path",
+            "source_id": 123,
+            "tags": ["x", "y"],
+            "trace": ["a", "b"],
+            "user_id": null,
+            "context": [
+                {
+                    "key": "foo",
+                    "value": "bar"
+                }
+            ]
+        }';
+    }
+
+    public function testSerialize(): void
+    {
+        $chan = DTO\Log::createFromJSON($this->json);
+        $serialized = json_encode($chan);
+        $this->assertJsonStringEqualsJsonString($this->json, $serialized);
+    }
+
+    public function testInheritance(): void
+    {
+        $c = DTO\Log::createFromJSON($this->json);
+        $this->assertLog($c);
+        $c = new DTO\Log([
+            'client_id' => 21,
+            'log_to_es' => true,
+            'level' => 'info',
+            'message' => 'x',
+            'origin' => 'y',
+        ]);
+        $this->assertLogNull($c);
+    }
+
+    private function assertLog(DTO\Log $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Log', $c);
+        $this->assertIsArray($c->tags);
+        $this->assertIsArray($c->trace);
+        $this->assertIsArray($c->context);
+    }
+
+    private function assertLogNull(DTO\Log $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Log', $c);
+        $this->assertEmpty($c->tags);
+        $this->assertEmpty($c->trace);
+        $this->assertEmpty($c->context);
+    }
+}
