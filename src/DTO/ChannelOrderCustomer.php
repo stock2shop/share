9d82848c410b1eaf47ff3aed0005e49208cfba23
Arata Yano <aratayano00@gmail.com>
2022-10-24 12:43:43 +0200

    channel order customer

---
+++ src/DTO/ChannelOrderCustomer.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stock2Shop\Share\DTO;
+
+use JsonSerializable;
+use stock2shop\vo\Address;
+
+class ChannelOrderCustomer extends DTO implements JsonSerializable, DTOInterface
+{
+    public ?int $email;
+    public ?int $first_name;
+    public ?int $last_name;
+    public ?bool $accepts_marketing;
+
+    public function __construct(array $data)
+    {
+        $this->accepts_marketing = self::boolFrom($data, 'accepts_marketing');
+        $this->email             = self::stringFrom($data, 'email');
+        $this->first_name        = self::stringFrom($data, 'first_name');
+        $this->last_name         = self::stringFrom($data, 'last_name');
+    }
+
+    public function jsonSerialize(): array
+    {
+        return (array)$this;
+    }
+
+    public static function createFromJSON(string $json): ChannelOrderCustomer
+    {
+        $data = json_decode($json, true);
+        return new ChannelOrderCustomer($data);
+    }
+
+    /**
+     * @return ChannelOrderCustomer[]
+     */
+    public static function createArray(array $data): array
+    {
+        $a = [];
+        foreach ($data as $item) {
+            $a[] = new ChannelOrderCustomer((array)$item);
+        }
+        return $a;
+    }
+}
