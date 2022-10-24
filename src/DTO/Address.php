2487f20ca85fd7beff0986128b31a87a86f873f8
Arata Yano <aratayano00@gmail.com>
2022-10-24 12:43:19 +0200

    address

--- src/DTO/Address.php
+++ src/DTO/Address.php
@@ -10,38 +10,32 @@ class Address extends DTO implements JsonSerializable, DTOInterface
 {
     public ?string $address1;
     public ?string $address2;
-    public ?string $address_code;
     public ?string $city;
-    public ?string $country_code;
     public ?string $company;
     public ?string $country;
-    public ?bool $default;
     public ?string $first_name;
     public ?string $last_name;
     public ?string $phone;
     public ?string $province;
-    public ?string $province_code;
     public ?string $zip;
-    public ?string $type;
-
+    public ?string $country_code;
+    public ?string $province_code;
 
     public function __construct(array $data)
     {
         $this->address1      = self::stringFrom($data, 'address1');
         $this->address2      = self::stringFrom($data, 'address2');
-        $this->address_code  = self::stringFrom($data, 'address_code');
         $this->city          = self::stringFrom($data, 'city');
-        $this->country_code  = self::stringFrom($data, 'country_code');
         $this->company       = self::stringFrom($data, 'company');
         $this->country       = self::stringFrom($data, 'country');
-        $this->default       = self::boolFrom($data, 'default');
         $this->first_name    = self::stringFrom($data, 'first_name');
         $this->last_name     = self::stringFrom($data, 'last_name');
         $this->phone         = self::stringFrom($data, 'phone');
         $this->province      = self::stringFrom($data, 'province');
-        $this->province_code = self::stringFrom($data, 'province_code');
-        $this->type          = self::stringFrom($data, 'type');
         $this->zip           = self::stringFrom($data, 'zip');
+        $this->country_code  = self::stringFrom($data, 'country_code');
+        $this->province_code = self::stringFrom($data, 'province_code');
+
     }
 
     public static function createFromJSON(string $json): Address
