97e0c00fa6e7feb8afed63d40e6c51126d7cda4e
Arata Yano <aratayano00@gmail.com>
2022-10-26 13:36:28 +0200

    SystemOrderShippingLine

---
+++ tests/DTO/SystemOrderShippingLineTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stock2Shop\Tests\Share\DTO;
+
+use PHPUnit\Framework\TestCase;
+use Stock2Shop\Share\DTO;
+
+class SystemOrderShippingLineTest extends TestCase
+{
+    private string $json;
+
+    protected function setUp(): void
+    {
+        $this->json = '
+        {
+            "price": 99.99,
+            "title": "title",
+            "channel_id": 1,
+            "client_id": 2,
+            "created": "created",
+            "modified": "modified",
+            "price_display": "price_display",
+            "sub_total": 3.01,
+            "sub_total_display": "sub_total_display",
+            "tax": 4.02,
+            "tax_display": "tax_display",
+            "tax_per_unit": 5.03,
+            "tax_per_unit_display": "tax_per_unit_display",
+            "total": 6.04,
+            "total_discount": 7.05,
+            "total_discount_display": "total_discount_display",
+            "total_display": "total_display"
+        }';
+    }
+
+    public function testSerialize(): void
+    {
+        $m = DTO\SystemOrderShippingLine::createFromJSON($this->json);
+        $serialized = json_encode($m);
+        $this->assertJsonStringEqualsJsonString($this->json, $serialized);
+    }
+
+    public function testInheritance(): void
+    {
+        $m = DTO\SystemOrderShippingLine::createFromJSON($this->json);
+        $this->assertSystemOrderShippingLine($m);
+    }
+
+    private function assertSystemOrderShippingLine(DTO\SystemOrderShippingLine $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\SystemOrderShippingLine', $c);
+    }
+}
