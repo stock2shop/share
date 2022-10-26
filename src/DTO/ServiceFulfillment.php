c2dae84b19656fc14e6a7a278633405cdf0bc045
Arata Yano <aratayano00@gmail.com>
2022-10-26 11:55:44 +0200

    ServiceFulfillment should not override the line items

--- src/DTO/ServiceFulfillment.php
+++ src/DTO/ServiceFulfillment.php
@@ -8,16 +8,10 @@ use JsonSerializable;
 
 class ServiceFulfillment extends Fulfillment implements JsonSerializable, DTOInterface
 {
-    /** @var FulfillmentLineItem[] $line_items */
-    public array $line_items;
 
     public function __construct(array $data)
     {
         parent::__construct($data);
-
-        $line_items = FulfillmentLineItem::createArray(self::arrayFrom($data, 'tax_lines'));
-
-        $this->line_items = self::sortArray($line_items, 'sku');
     }
 
     public static function createFromJSON(string $json): ServiceFulfillment
---
+++ tests/DTO/ServiceFulfillmentTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stock2Shop\Tests\Share\DTO;
+
+use PHPUnit\Framework\TestCase;
+use Stock2Shop\Share\DTO;
+
+class FulfillmentTest extends TestCase
+{
+    private string $json;
+
+    protected function setUp(): void
+    {
+        $this->json = '
+        {
+            "fulfillmentservice_order_code": "fulfillmentservice_order_code",
+            "line_items": [
+                {
+                    "grams": 10,
+                    "qty": 1,
+                    "sku": "sku",
+                    "fulfilled_qty": 0
+                }
+            ],
+            "notes": "notes",
+            "state": "state",
+            "status": "status",
+            "tracking_company": "tracking_company",
+            "tracking_number": 123,
+            "tracking_url": "tracking_url"
+        }';
+    }
+
+    public function testSerialize(): void
+    {
+        $m = DTO\Fulfillment::createFromJSON($this->json);
+        $serialized = json_encode($m);
+        $this->assertJsonStringEqualsJsonString($this->json, $serialized);
+    }
+
+    public function testInheritance(): void
+    {
+        $m = DTO\Fulfillment::createFromJSON($this->json);
+        $this->assertFulfillment($m);
+    }
+
+    private function assertFulfillment(DTO\Fulfillment $c)
+    {
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\DTO', $c);
+        $this->assertInstanceOf('Stock2Shop\Share\DTO\Fulfillment', $c);
+    }
+}
